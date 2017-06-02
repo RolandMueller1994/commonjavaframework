cc291379d5de4e2bf53fa49c5336a2fc7b1e8468
RolandMueller1994 <roland.mueller.1994@freenet.de>
2017-06-02 17:02:41 +0200

    GlobalResourceProviderTest

--- commonjavaframework/src/logging/AbstractLogger.java
+++ commonjavaframework/src/logging/AbstractLogger.java
@@ -2,18 +2,10 @@ package logging;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.PrintWriter;
-import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
-
 import resourceframework.GlobalResourceProvider;
 import resourceframework.ResourceProviderException;
 
---
+++ commonjavaframework/test/commonjavaframework/LoggerTest.java
@@ -0,0 +1,14 @@
+package commonjavaframework;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoggerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
