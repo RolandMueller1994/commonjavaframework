24bec798fc76a2f9b050c9e49e0d940ee1d2bc31
RolandMueller1994 <roland.mueller.1994@freenet.de>
2017-06-03 12:05:34 +0200

    Import GradleProject

---
+++ commonjavaframework/src/main/java/resourceframework/ResourceProviderException.java
@@ -0,0 +1,18 @@
+package resourceframework;
+
+public class ResourceProviderException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7241263236493188393L;
+
+	public ResourceProviderException() {
+		
+	}
+	
+	public ResourceProviderException(String message) {
+		super(message);
+	}
+	
+}
