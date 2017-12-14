15a0eec7799f8add3022204bc5bdda7b578b89de
RolandMueller1994 <roland.mueller.1994@freenet.de>
2017-12-14 22:53:46 +0100

    Javadoc

--- commonjavaframework/src/main/java/guicomponents/AbstractNumberTextField.java
+++ commonjavaframework/src/main/java/guicomponents/AbstractNumberTextField.java
@@ -9,20 +9,29 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
+/**
+ * Abstract class for {@link TextField}s which should only display numbers.
+ * Subclasses have to define which data type is supported and how insertes
+ * strings have to be checked.
+ * 
+ * @author roland
+ *
+ * @param <T> the data type which should be handled
+ */
 public abstract class AbstractNumberTextField<T extends Number> extends TextField {
 
 	private T minValue;
 	private T maxValue;
-	
+
 	private T value;
-	
+
 	private HashSet<ValueChangedInterface> listeners = new HashSet<>();
-	
+
 	protected AbstractNumberTextField(T value, T minValue, T maxValue) {
 		this.minValue = minValue;
 		this.maxValue = maxValue;
 		setValue(value);
-		
+
 		textProperty().addListener(new ChangeListener<String>() {
 			@Override
 			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
@@ -30,7 +39,7 @@ public abstract class AbstractNumberTextField<T extends Number> extends TextFiel
 				setText(checkInput(newValue));
 			}
 		});
-		
+
 		setOnKeyPressed(new EventHandler<KeyEvent>() {
 			@Override
 			public void handle(KeyEvent key) {
@@ -44,46 +53,62 @@ public abstract class AbstractNumberTextField<T extends Number> extends TextFiel
 			}
 		});
 	}
-	
+
+	/**
+	 * Set the value of the text field
+	 * @param value the new value.
+	 */
 	public void setValue(T value) {
 		this.value = checkValue(value);
 		setText(this.value.toString());
 	}
-	
+
 	private T checkValue(T value) {
-		if(value.doubleValue() > maxValue.doubleValue()) {
+		if (value.doubleValue() > maxValue.doubleValue()) {
 			return maxValue;
-		} else if(value.doubleValue() < minValue.doubleValue()) {
+		} else if (value.doubleValue() < minValue.doubleValue()) {
 			return minValue;
 		}
 		return value;
 	}
-	
+
+	/**
+	 * Get the value which is currently valid.
+	 * @return the valid value.
+	 */
 	public T getValue() {
 		return value;
 	}
-	
+
+	/**
+	 * Registered listeners will be called if the valid value changes.
+	 * @param listener the {@link ValueChangedInterface} to call.
+	 */
 	public void registerValueChangedListener(ValueChangedInterface listener) {
 		listeners.add(listener);
 	}
-	
+
+	/**
+	 * Remove a listener
+	 * @param listener the {@link ValueChangedInterface} to remove.
+	 */
 	public void removeValueChangedListener(ValueChangedInterface listener) {
 		listeners.remove(listener);
 	}
-	
+
 	private void fireValueChanged() {
-		for(ValueChangedInterface listener : listeners) {
+		for (ValueChangedInterface listener : listeners) {
 			listener.valueChanged(value);
 		}
 	}
-	
+
 	protected abstract String checkInput(String input);
-	
+
 	protected abstract T parseInput(String input);
-	
+
 	public interface ValueChangedInterface {
-		
+
 		public void valueChanged(Number value);
-		
+
 	}
 }
